16fef8c19d9ea5b1384e0141cf935ad670dbbe07
Thor99 <thor.garcia@pagar.me>
2019-05-27 09:02:51 -0300

    function-as-values: add annotations

---
+++ functions-as-values.js
@@ -0,0 +1,16 @@
+var foo = function() {
+	// ..
+}
+
+var x = function bar(){
+	// ..
+};
+
+/*
+
+The first function expression assigned to the foo variable is called anonymous because it has no name.
+
+The second function expression is named (bar), even as a reference to it is also assigned to the x variable. Named function expressions are generally more 
+preferable, though anonymous function expressions are still extremely common.
+
+*/ 
